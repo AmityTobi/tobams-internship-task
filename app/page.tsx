e2be52bb8d878db89ae35d7d98b4183455811259
amitytobi <amityekoyi@gmail.com>
2026-09-05 17:17:24 +0100

    import and render LMSBlock section in main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Navbar from "@/components/navbar";
 import Hero from "@/components/hero";
+import LMSBlock from "@/components/lmsBlock";
 
 export default function Home() {
   return (
     <main className="min-h-screen">
       <Navbar />
       <Hero />
+      <LMSBlock />
     </main>
   );
 }
