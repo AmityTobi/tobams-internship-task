586d3b3027fe4d98bbc3cdd7f38cf21488113db7
amitytobi <amityekoyi@gmail.com>
2026-09-05 20:26:21 +0100

    integrate ManagementProgram component into main page

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Navbar from "@/components/navbar";
 import Hero from "@/components/hero";
 import LMSBlock from "@/components/lmsBlock";
 import ServicesSection from "@/components/servicesSection";
+import ManagementProgram from "@/components/managementProgram";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <Hero />
       <LMSBlock />
       <ServicesSection />
+      <ManagementProgram />
     </main>
   );
 }
